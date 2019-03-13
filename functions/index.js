cecf7059971bfd8ffbed2d4462c47f674aa36f37
b0ho <ybg4828@gmail.com>
2019-03-13 23:56:00 +0900

    완성

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+app.engine("html", require("ejs").renderFile);
+app.set('views', './views');
+app.set("view engine", "ejs");
+
+var XLSX = require("xlsx");
+var workbook = XLSX.readFile("asset/wordbook.xlsx");
+var firstSheetName = workbook.SheetNames[0];
+var firstSheet = workbook.Sheets[firstSheetName];
+var set = XLSX.utils.sheet_to_json(firstSheet, { raw: true });
+
+var alph = [];
+var phon = [];
+var han = [];
+
+for (var i in set) {
+  alph[i] = set[i].s_alpha;
+  phon[i] = set[i].s_phonetic;
+  han[i] = set[i].s_hangeul;
+}
+
+app.get("/", (req, res) => {
+  res.render("index.html", {
+    alph: alph,
+    phon: phon,
+    han: han
+    
+  });
+});
+
+exports.app = functions.https.onRequest(app);
